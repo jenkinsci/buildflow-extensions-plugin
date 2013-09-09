484879a678c68864907a6c6a176281cc1f29ceae
James Nord <jnord@cisco.com>
2013-09-09 10:04:52 +0100

    Add a hyperlink to the most recent flow if we can't get the lock.

--- src/main/groovy/org/jenkinsci/plugins/buildflow/concurrent/extension/ConcurrentBuildFlowDSL.groovy
+++ src/main/groovy/org/jenkinsci/plugins/buildflow/concurrent/extension/ConcurrentBuildFlowDSL.groovy
@@ -25,10 +25,15 @@ package org.jenkinsci.plugins.buildflow.concurrent.extension
 
 import com.cloudbees.plugins.flow.FlowDelegate
 import com.cloudbees.plugins.flow.FlowRun
+import com.cloudbees.plugins.flow.JobNotFoundException
 import hudson.AbortException
+import hudson.console.HyperlinkNote
+import hudson.model.AbstractProject
 import hudson.model.Executor
+import hudson.model.ItemGroup
 import hudson.model.Result
 import hudson.model.Run
+import jenkins.model.Jenkins
 
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
@@ -44,6 +49,25 @@ public class ConcurrentBuildFlowDSL {
 		this.flowDelegate = flowDelegate;
 	}
 
+/**
+ * Get the most recent stable build of a project.
+ * @param project name of the project (relative to this flow).
+ * @throws JobNotFoundException if the job can not be located, or there is no lastStable build.
+ */
+	def getLastStableRun(String project) {
+		ItemGroup context = flowDelegate.build.getProject().getParent()
+		def proj  = Jenkins.instance.getItem(project, (ItemGroup) context, AbstractProject.class)
+		if (proj  == null) {
+			throw new JobNotFoundException("Item ${project} not found (or isn't a job).")
+		}
+		def run = proj.lastStableBuild
+		if (run == null) {
+			throw new JobNotFoundException("Item ${project} has no lastStable run.")
+		}
+		return run
+	}
+
+
 	/**
 	 * The block DSL ensures that only one FlowRun of a particular BuildFlow will execute the steps contained within it concurrently.
 	 * It further will only allow the most recent FlowRun to proceed if multiple become blocked.
@@ -52,14 +76,14 @@ public class ConcurrentBuildFlowDSL {
 	 * @return <code>true</code>ue if the block was executed, <code>false</code> if the block was nor run.
 	 */
 	def boolean block(String blockName, innerBlock) {
-
+		flowDelegate.statusCheck()
 		// due to the nature of any jobs and scheduling the order of items inserted may not be
 		// ordered by build number so use an explicit comparator.
 		SortedSet waiting = getSharedState(blockName + "_WAITING_LIST",
 		                            Collections.synchronizedSortedSet(new TreeSet()))
 		ReentrantLock lock = getSharedState(blockName + "_LOCK", new ReentrantLock())
 
-		waiting.add(flowDelegate.flowRun);
+		waiting.add(flowDelegate.flowRun)
 		// try and get the lock.
 		try {
 			flowDelegate.println("block ($blockName) {")
@@ -69,25 +93,24 @@ public class ConcurrentBuildFlowDSL {
 				boolean locked = lock.tryLock(1L, TimeUnit.SECONDS)
 				FlowRun next = waiting.last();
 				if (next != flowDelegate.flowRun) {
-					flowDelegate.println("// [${flowDelegate.flowRun}] a more recent FlowRun ${next} is requesting the same" +
-							"lock (${blockName}) - not progressing with the build")
+					flowDelegate.println("// [${flowDelegate.flowRun}] a more recent FlowRun " +
+					                     HyperlinkNote.encodeTo('/'+ next.getUrl(), next.getFullDisplayName()) +
+					                     " is requesting the same lock (${blockName}) - not progressing with the build")
 					// if there is a newer waiting FlowRun then remove ourselves and abort
 					waiting.remove(flowDelegate.flowRun)
-					/*
-					  // how do we abort (stop the flow progressing any further?) the build- shall we try to interrupt ourselves!
-					  // should we return true/false and get the caller to handle it?
-					  Thread.currentThread().interrupt();
-					  break;
-					  */
 					if (locked) {
 						lock.unlock()
 					}
+					/*
+					 * Stop the build progressing by aborting the build.
+					 * Future: perhaps signal this by boolean return?
+					 */
 					def abortCause = new MoreRecentFlowAbortCause(next, blockName)
 					Executor.currentExecutor().interrupt(Result.ABORTED, abortCause)
 					throw new AbortException(abortCause.shortDescription)
 				}
 				if (locked) {
-					flowDelegate.println("// block ($blockName) acquired by ${flowDelegate.flowRun}")
+					flowDelegate.println("// lock ($blockName) acquired by ${flowDelegate.flowRun}")
 					// we know there is not a newer BuildFlow than us!
 					// (actually there is a small race condition here - but not worth extra locking).
 					innerBlock()
